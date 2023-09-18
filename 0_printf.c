7347fc4ad155164a1732f1685f35f78b8a59a6a1
Wache098 <wacheram22@gmail.com>
2023-09-18 09:00:26 -0700

    my printf

---
+++ 0_printf.c
@@ -0,0 +1,114 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_char - prints out a character
+ * @args:list of arguments
+ *
+ * Return:no of printed characters.
+ */
+
+int print_char(va_list args)
+{
+int c;
+
+c = va_arg(args, int);
+putchar(c);
+return (1);
+}
+/**
+ * print_string - prints a string
+ * @args:list of arguments
+ *
+ * Return:no of characters printed
+ */
+int print_string(va_list args)
+{
+char *str = va_arg(args, char *);
+int count;
+
+if (str == NULL)
+{
+	printf("(null)");
+	return (6);
+}
+else
+{
+
+count = 0;
+
+while (*str)
+{
+putchar(*str);
+str++;
+count++;
+}
+return (count);
+}
+}
+
+/**
+ * handle_format_specifier - individual format specifiers
+ * @format:pointer to current format specifier
+ * @args:list of arguments
+ *
+ * Return:no of chars printed
+ */
+int handle_format_specifier(const char **format, va_list args)
+{
+int count;
+
+count = 0;
+switch (**format)
+{
+	case 'c':
+		count += print_char(args);
+		break;
+	case 's':
+		count += print_string(args);
+		break;
+	case '%':
+		count += print_string(args);
+		putchar('%');
+		count++;
+		break;
+	default:
+		putchar('%');
+		putchar(**format);
+		count += 2;
+}
+return (count);
+}
+/**
+ * _printf -prints output depending on format
+ * @format:format str with specifiers
+ *
+ * Return:no of char printed except null byte
+ */
+
+int _printf(const char *format, ...)
+{
+va_list args;
+int count;
+
+va_start(args, format);
+
+count = 0;
+
+while (*format)
+{
+if (*format != '%')
+{
+putchar(*format);
+count++;
+}
+else
+{
+format++;
+count += handle_format_specifier(&format, args);
+}
+format++;
+}
+va_end(args);
+return (count);
+}
