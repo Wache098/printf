2edc49bbb1176b02f755f12c3cd90cda9609a415
Wache098 <wacheram22@gmail.com>
2023-09-18 12:24:07 -0700

    made a tests folder for main.c files

--- main.h
+++ main.h
@@ -2,6 +2,13 @@
 #define MAIN_H
 
 #include <stdarg.h>
+#include <stddef.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
+int print_char(va_list args);
+int handle_format_specifier(const char **format, va_list args);
+int print_string(va_list args);
+int _putchar(char c);
+
 #endif
